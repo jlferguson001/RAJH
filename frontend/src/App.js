692463fd81eae72b5976b37e693a5fc7a0684b47
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-22 10:54:38 -0500

    homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
- 
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
+
   return (
     <>
-      <h1> Welcome to RAJH Frames</h1>
+      <Header />
+      <main className = 'py-4'>
+        <Container>
+          <h1> Welcome to RAJH Frames</h1>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+ 
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>RAJH Frames</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link to="/">Home</Nav.Link>
+              <Nav.Link to="/Mens" > Men's </Nav.Link>
+              <Nav.Link to="/Womens">
+                Women's
+              </Nav.Link>
+              <Nav.Link to="/Childrens">
+                Children's
+              </Nav.Link>
+              <Nav.Link to='/sunglasses'>Sunglasses</Nav.Link>
+            </Nav>
+            <Nav className='ms-auto'>
+              {/* importing icons from font awesome, import was saved in index.html */}
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>Log In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+      )
+}
+
+export default Header
