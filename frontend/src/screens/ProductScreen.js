9f64fcff0ca0f7692b348a59c5d2b53115c871d1
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-22 17:24:30 -0500

    product detail

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({match}) => {
+    // when getting data from front end
+    const product = products.find(p => p._id === match.params.id)
   return (
-    <div>ProductScreen</div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        {' '}
+        Go Back{' '}
+      </Link>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+
+        <Col md={4}>
+          {/* flush takes away the border */}
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>Model:{'  ' + product.name}</h3>
+              <h4>{product.category}</h4>
+              <h5>Color:{'  ' + product.color}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h3>Price ${product.price}</h3>
+              <Button className='btn' type='button'>
+                {' '}
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
   )
 }
 
