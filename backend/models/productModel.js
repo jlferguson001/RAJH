47b6144348b15af074c067c55691770372c5e180
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-08-23 07:11:45 -0500

    updated product schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,28 +3,33 @@ import mongoose from 'mongoose'
 const reviewSchema = mongoose.Schema({
     name: { type: String, required: true},
     rating: { type: Number, required: true},
-    name: { type: String, required: true}
+    comment: { type: String, required: true}
+}, {
+    timestamps: true
 })
 
-const productSchema = mongoose.Schema(
-  {
+const productSchema = mongoose.Schema({
     user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: 'User',
     },
+
     name: {
       type: String,
       required: true,
     },
+
     image: {
       type: String,
       required: true,
     },
+
     description: {
       type: String,
       required: false,
     },
+
     brand: {
       type: String,
       required: true,
@@ -34,24 +39,28 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+
     sunglasses: {
       type: Boolean,
       required: true,
     },
+
     color: {
       type: String,
       required: true,
     },
+
     price: {
       type: Numeric,
       required: true,
       deafult: 0,
     },
-    },
+    
     countInStock: {
       type: Numeric,
       required: true,
     },
+
     reviews: [reviewSchema],
 
     rating: {
@@ -59,13 +68,19 @@ const productSchema = mongoose.Schema(
       required: true,
       default: 0,
     },
+
     numReviews: {
       type: Number,
       required: true,
       default: 0,
     },
-  },
+},
   {
     timestamps: true,
   }
+
 )
+
+const Product = mongoose.model( 'Product', productSchema)
+
+export default Product
